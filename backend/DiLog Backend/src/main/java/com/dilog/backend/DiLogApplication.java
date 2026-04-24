66574e9a5cfe4ef7abe4bb97136be957e094a945
samallday11 <samuelhabtamu875@gmail.com>
2026-04-24 14:47:09 -0500

    add main application class

---
+++ backend/DiLog Backend/src/main/java/com/dilog/backend/DiLogApplication.java
@@ -0,0 +1,11 @@
+package com.dilog.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiLogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiLogApplication.class, args);
+    }
+}
